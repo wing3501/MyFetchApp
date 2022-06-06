5f377eecc054ad61b7a15699bced6330c79f4368
申屠云飞 <shentyf@weipaitang.com>
2022-06-06 16:15:42 +0800

    提交代码

--- MyFetchApp/Module/YuShe/dytt/View/DyttItemRow.swift
+++ MyFetchApp/Module/YuShe/dytt/View/DyttItemRow.swift
@@ -24,10 +24,11 @@ struct DyttItemRow: View {
             Text(itemModel.desc)
                 .font(.body)
                 .foregroundColor(.blue)
-//                .background(.random)
+////                .background(.random)
                 .lineLimit(isExpanding ? nil : 1)
         }
-        .animation(.default, value: isExpanding)
+//        .frame(height: isExpanding ? 220 : 60, alignment: .top)
+//        .animation(.default, value: isExpanding)
         .onTapGesture {
 //            withAnimation { //显式动画导致动画弹跳
                 isExpanding.toggle()
@@ -35,8 +36,6 @@ struct DyttItemRow: View {
         }
     }
 }
-
-
 
 
 struct DyttItemRow_Previews: PreviewProvider {
@@ -49,7 +48,9 @@ struct DyttItemRow_Previews_ContentView: View {
     @State var isExpanding = false
     var body: some View {
         List {
-            DyttItemRow(itemModel: DyttItemModel(title: "1", subTitle: "2", desc: "3", href: ""))
+            DyttItemRow(itemModel: DyttItemModel(title: "2022年剧情爱情《我是真的讨厌异地恋》HD国语中字", subTitle: "日期：2022-06-02 19:05:53 点击：0", desc: "◎译 名 Stay with Me ◎片 名 我是真的讨厌异地恋 ◎年 代 2022 ◎产 地 中国大陆 ◎类 别 剧情/爱情 ◎语 言 普通话 ◎字 幕 中文 ◎上映日期 2022-04-29(中国大陆) ◎豆瓣评分 5.0/10 from 10927 users ◎片 长 108分钟 ◎导 演 吴洋 Yang Wu 周男燊 Nanshen Zhou", href: ""))
+            DyttItemRow(itemModel: DyttItemModel(title: "2022年剧情爱情《我是真的讨厌异地恋》HD国语中字", subTitle: "日期：2022-06-02 19:05:53 点击：0", desc: "◎译 名 Stay with Me ◎片 名 我是真的讨厌异地恋 ◎年 代 2022 ◎产 地 中国大陆 ◎类 别 剧情/爱情 ◎语 言 普通话 ◎字 幕 中文 ◎上映日期 2022-04-29(中国大陆) ◎豆瓣评分 5.0/10 from 10927 users ◎片 长 108分钟 ◎导 演 吴洋 Yang Wu 周男燊 Nanshen Zhou", href: ""))
+            DyttItemRow(itemModel: DyttItemModel(title: "2022年剧情爱情《我是真的讨厌异地恋》HD国语中字", subTitle: "日期：2022-06-02 19:05:53 点击：0", desc: "◎译 名 Stay with Me ◎片 名 我是真的讨厌异地恋 ◎年 代 2022 ◎产 地 中国大陆 ◎类 别 剧情/爱情 ◎语 言 普通话 ◎字 幕 中文 ◎上映日期 2022-04-29(中国大陆) ◎豆瓣评分 5.0/10 from 10927 users ◎片 长 108分钟 ◎导 演 吴洋 Yang Wu 周男燊 Nanshen Zhou", href: ""))
         }
     }
 }
