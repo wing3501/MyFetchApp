a89a212aed581bdd19d21163529e838ad5dbf618
申屠云飞 <shentyf@weipaitang.com>
2022-06-22 17:28:14 +0800

    提交代码

--- MyFetchApp/Module/YuShe/Magnet/MagnetView.swift
+++ MyFetchApp/Module/YuShe/Magnet/MagnetView.swift
@@ -9,11 +9,51 @@ import SwiftUI
 
 struct MagnetView: View {
     
-    @EnvironmentObject var store: Store
+//    @EnvironmentObject var store: Store
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .hideTabView($store.appState.hideTabView)
+        VStack {
+            HStack {
+                Button {
+                } label: {
+                    Text("从相册选择")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: .infinity, minHeight: 50)
+                .background(.green)
+                .buttonStyle(BorderlessButtonStyle())
+                
+                Button {
+                    
+                } label: {
+                    Text("扫一扫")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(BorderlessButtonStyle())
+                .frame(maxWidth: .infinity, minHeight: 50)
+                .background(.orange)
+                .buttonStyle(BorderlessButtonStyle())
+            }
+            .padding()
+            .padding(.top,80)
+            GroupBox("结果如下(点击可复制)：") {
+                VStack(alignment: .leading) {
+                    Text("Username: tswift89")
+                    Text("City: Nashville")
+                    GroupBox {
+                        Text("City: Nashville")
+                    }
+                }
+            }
+            .padding()
+            Spacer()
+        }
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .hideTabView($store.appState.hideTabView)
     }
 }
 
