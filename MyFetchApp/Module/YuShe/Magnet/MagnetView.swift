8d78669f8a6b656ccae1c2cdfa8bd6608afafcb8
申屠云飞 <shentyf@weipaitang.com>
2022-06-22 20:21:45 +0800

    提交代码

--- MyFetchApp/Module/YuShe/Magnet/MagnetView.swift
+++ MyFetchApp/Module/YuShe/Magnet/MagnetView.swift
@@ -6,37 +6,24 @@
 //
 
 import SwiftUI
+import SwiftUIX
 
 struct MagnetView: View {
     
 //    @EnvironmentObject var store: Store
     
+    @State var isShowPhotoLibrary = false
+    @State var image: UIImage?
+    
     var body: some View {
         VStack {
             HStack {
-                Button {
-                } label: {
-                    Text("从相册选择")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
+                MagnetViewButton(title: "从相册选择") {
+                    isShowPhotoLibrary.toggle()
                 }
-                .frame(maxWidth: .infinity, minHeight: 50)
-                .background(.green)
-                .buttonStyle(BorderlessButtonStyle())
-                
-                Button {
+                MagnetViewButton(title: "扫一扫") {
                     
-                } label: {
-                    Text("扫一扫")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
                 }
-                .buttonStyle(BorderlessButtonStyle())
-                .frame(maxWidth: .infinity, minHeight: 50)
-                .background(.orange)
-                .buttonStyle(BorderlessButtonStyle())
             }
             .padding()
             .padding(.top,80)
@@ -45,15 +32,44 @@ struct MagnetView: View {
                     Text("Username: tswift89")
                     Text("City: Nashville")
                     GroupBox {
-                        Text("City: Nashville")
+                        if let _ = image {
+                            Text("图片有值")
+                        }else {
+                            Text("图片为空")
+                        }
+                        
                     }
                 }
             }
             .padding()
             Spacer()
         }
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(image: $image, encoding: nil) {
+                print("取消了------")
+            }
+        }
 //            .hideTabView($store.appState.hideTabView)
+    }
+}
+
+struct MagnetViewButton: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Text(title)
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, minHeight: 50)
+        .background(.green)
+        .buttonStyle(BorderlessButtonStyle())
+        .cornerRadius(8)
     }
 }
 
---
+++ MyFetchApp/Tool/VNDetectManager.swift
@@ -0,0 +1,12 @@
+//
+//  VNDetectManager.swift
+//  MyFetchApp
+//
+//  Created by styf on 2022/6/22.
+//
+
+import Foundation
+
+class VNDetectManager {
+    
+}
