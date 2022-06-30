716e493ce846c69dbbb47da1238acc970dad1901
申屠云飞 <shentyf@weipaitang.com>
2022-06-30 14:20:58 +0800

    提交代码

--- MyFetchApp/UIKit/SUWebView.swift
+++ MyFetchApp/UIKit/SUWebView.swift
@@ -48,16 +48,50 @@ class SUWebViewDelegate: NSObject {
 }
 
 extension SUWebViewDelegate: WKUIDelegate {
-//    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo) async {
-//        //由原生来唤起弹窗
-//    }
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo) async {
+        //由原生来唤起弹窗
+//        let alertVC = UIAlertController(title: "提示", message: message, preferredStyle: .alert)
+//        alertVC.addAction(UIAlertAction(title: "确认", style: .default, handler: { action in
+//            completionHandler()
+//        }))
+//        present(alertVC, animated: true)
+    }
+    
+    //调用 JS 的 confirm 方法
+    func webView(_ WebView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+//        let alertVC = UIAlertController(title: "提示", message: message, preferredStyle: .alert)
+//        alertVC.addAction(UIAlertAction(title: "确认", style: .default, handler: { action in
+//            completionHandler(true)
+//        }))
+//        alertVC.addAction(UIAlertAction(title: "取消", style: .cancel, handler: { action in
+//            completionHandler(false)
+//        }))
+//        present(alertVC, animated: true)
+    }
     
 //    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo) async -> String? {
-//        //客户端可以通过下面的方法获取到输入框事件，并由客户端展示输入框，用户输入完成后将结果回调给completionHandler中。
+        //客户端可以通过下面的方法获取到输入框事件，并由客户端展示输入框，用户输入完成后将结果回调给completionHandler中。
+        
+//        let alertVC = UIAlertController(title: prompt, message: "", preferredStyle: .alert)
+//        alertVC.addTextField { textFiled in
+//            textFiled.text = defaultText
+//        }
+//        alertVC.addAction(UIAlertAction(title: "完成", style: .default, handler: { action in
+//            completionHandler(alertVC.textFields?[0].text ?? "")
+//        }))
+//        present(alertVC, animated: true)
 //    }
     
     func webViewDidClose(_ webView: WKWebView) {
-        print("webViewDidClose...")
+        print("通知 App，DOM 窗口已成功关闭 webViewDidClose...")
+    }
+    
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+//        经常用于在项目中处理 H5 界面中含有 target = __blank 标签（表示新建一个页面打开网页）或者网页中点击无响应的情况，其处理逻辑主要是判断目标主视图是否为空，如果不为空则允许导航。
+        if navigationAction.targetFrame?.isMainFrame == nil {
+            webView.load(navigationAction.request)
+        }
+        return nil
     }
 }
 
@@ -92,5 +126,9 @@ extension SUWebViewDelegate: WKNavigationDelegate {
 //                print("看看----\(dataString)")
 //            }
 //        }
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+//        开始获取到网页内容时返回
     }
 }
