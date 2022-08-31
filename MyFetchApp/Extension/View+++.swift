f991902bc081fe120503b7b30106d611c550e717
申屠云飞 <shentyf@weipaitang.com>
2022-08-31 11:14:57 +0800

    代码提交

--- MyFetchApp/Extension/View+++.swift
+++ MyFetchApp/Extension/View+++.swift
@@ -7,6 +7,42 @@
 
 import SwiftUI
 
+// MARK: - 通过GeometryReader获取环境值
+
+struct SafeAreaInsetsKey: PreferenceKey {
+    static var defaultValue = EdgeInsets()
+    static func reduce(value: inout EdgeInsets, nextValue: () -> EdgeInsets) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    // 获取 SafeAreaInsets
+    func getSafeAreaInsets(_ safeInsets: Binding<EdgeInsets>) -> some View {
+        background(
+            GeometryReader { proxy in
+                Color.clear
+                    .preference(key: SafeAreaInsetsKey.self, value: proxy.safeAreaInsets)
+            }
+            .onPreferenceChange(SafeAreaInsetsKey.self) { value in
+                safeInsets.wrappedValue = value
+            }
+        )
+    }
+    
+    func printSafeAreaInsets(id: String) -> some View {
+        background(
+            GeometryReader { proxy in
+                Color.clear
+                    .preference(key: SafeAreaInsetsKey.self, value: proxy.safeAreaInsets)
+            }
+            .onPreferenceChange(SafeAreaInsetsKey.self) { value in
+                print("\(id) insets:\(value)")
+            }
+        )
+    }
+}
+
 // MARK: - 遮罩视图
 
 extension View {
